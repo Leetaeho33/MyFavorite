8497fa93d7458c765c09e765355db0eb917ed63f
taeholee <lth1993@naver.com>
2023-12-21 14:51:25 +0900

    feat : scheduler를 이용해 게시글 자동 삭제 기능 구현

--- src/main/java/com/taeho/myfavorite/post/repository/PostRepository.java
+++ src/main/java/com/taeho/myfavorite/post/repository/PostRepository.java
@@ -3,8 +3,11 @@ package com.taeho.myfavorite.post.repository;
 import com.taeho.myfavorite.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByCreatedAtDesc();
+
+    void deleteByCreatedAtLessThanEqual(LocalDateTime localDateTime);
 }
